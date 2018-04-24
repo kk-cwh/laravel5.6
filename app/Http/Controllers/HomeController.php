6e560cf711b073e6f8d37ca2231360213c4d8ba8
jaak <zhangykvip@126.com>
2018-04-24 17:07:28 +0800

    add HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use App\Category;
+use App\Tag;
+use Illuminate\Http\Request;
+
+class HomeController extends ApiController
+{
+    //  文章类别
+    public function types()
+    {
+        $types = Category::all(['id', 'name']);
+        return $this->apiResponse(compact('types'));
+    }
+
+    // 文章标签
+    public function tags()
+    {
+        $tags = Tag::withCount('articles')->get(['id', 'name']);
+        return $this->apiResponse(compact('tags'));
+    }
+
+    // 文章
+    public function articles(Request $request)
+    {
+        $tagId = $request->input('tag_id');
+
+        $query = Article::with(['category', 'tags']);
+        if ($tagId) {
+            $query->whereHas('tags', function ($query) use ($tagId) {
+                $query->where('tags.id', $tagId);
+            });
+        }
+        $data = $query->orderBy('id', 'DESC')->paginate()->toArray();
+
+        $articles['total']        = array_get($data, 'total');
+        $articles['current_page'] = array_get($data, 'current_page');
+        $articles['last_page']    = array_get($data, 'last_page');
+        $articles['list']         = array_get($data, 'data');
+        return $this->apiResponse(compact('articles'));
+    }
+
+
+    public function articleDetail($id)
+    {
+        $article = Article::with(['type', 'tags'])->find($id);
+
+        return $this->apiResponse(compact('article'));
+    }
+
+    public function archives(Request $request)
+    {
+        $tagId = $request->input('tag_id');
+        $query = Article::with(['category', 'tags']);
+        if ($tagId) {
+            $query->whereHas('tags', function ($query) use ($tagId) {
+                $query->where('tags.id', $tagId);
+            });
+        }
+        $data                     = $query->select(DB::raw("id,type_id,title,created_at,DATE_FORMAT(`created_at`,'%M,%Y') as archives"))->orderBy('id', 'DESC')->paginate()->toArray();
+        $articles['total']        = array_get($data, 'total');
+        $articles['current_page'] = array_get($data, 'current_page');
+        $articles['last_page']    = array_get($data, 'last_page');
+        $articles['list']         = collect(array_get($data, 'data'))->groupBy('archives');
+        return $this->apiResponse(compact('articles'));
+    }
+}
